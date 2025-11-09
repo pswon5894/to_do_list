05da706c706c3257649273434b6ae72cf994ac0b
pswon5894 <pswon5894@gmail.com>
2025-11-09 22:27:37 +0900

    리드 미 추가

--- app/src/main/java/com/example/to_do_list/TodoRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/to_do_list/TodoRecyclerViewAdapter.kt
@@ -53,5 +53,7 @@ class TodoRecyclerViewAdapter (private val todoList : ArrayList<TodoEntity>, pri
 
 //class TodoRecyclerViewAdapter (private val todoList : ArrayList<TodoEntity>) : RecyclerView.Adapter<TodoRecyclerViewAdapter.MyViewHolder(){
 //뷰홀더 패턴-각 객체를 뷰폴더에 보관해서 반복 메서드 호출을 줄여 속도를 개선하는 패턴
+//view binding-View Binding은 XML 레이아웃에 정의된 뷰에 대해 자동으로 바인딩 클래스를 생성하여 `findViewById` 없이 직접 접근하게 해줍니다. 이로 인해 코드가 간결해지고 오류 발생 가능성이 줄어듭니다.
+
 //RecyclerView.Adapter 리사이클러뷰에서 어뎁터 상속
 //https://velog.io/@haero_kim/Android-ViewHolder-%ED%8C%A8%ED%84%B4%EC%9D%84-%EC%93%B0%EB%8A%94-%EC%9D%B4%EC%9C%A0
